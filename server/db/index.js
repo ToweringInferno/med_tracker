3257c23beeb368262e9c300dfa12f1ff9f3ece78
Jordan Estes <jle48@georgetown.edu>
2017-03-09 14:37:26 -0500

    Initial setup

---
+++ server/db/index.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+// var connection = mysql.createConnection({
+//   user: 'root',
+//   password: '',
+//   database: 'medschedules'
+// });
+
+// connection.connect();
+
+// module.exports = connection;
